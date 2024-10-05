c1edde9bfc9100076601bc94233d88274900bb7b
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-10-04 21:52:06 -0400

    Update abort-controller.js

--- abort-controller.js
+++ abort-controller.js
@@ -8,3 +8,18 @@ class AbortSignal extends eventTargetShim.EventTarget {
     /**
      * AbortSignal cannot be constructed directly.
      */
+    constructor() {
+        super();
+        throw new TypeError("AbortSignal cannot be constructed directly");
+    }
+    /**
+     * Returns `true` if this `AbortSignal`'s `AbortController` has signaled to abort, and `false` otherwise.
+     */
+    get aborted() {
+        const aborted = abortedFlags.get(this);
+        if (typeof aborted !== "boolean") {
+            throw new TypeError(`Expected 'this' to be an 'AbortSignal' object, but got ${this === null ? "null" : typeof this}`);
+        }
+        return aborted;
+    }
+}
