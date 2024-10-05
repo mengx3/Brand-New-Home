de20dd4e911ca6ad9d656ea7d4f3ef8462539b3f
阿娟 <wangzhiyuanstan@outlook.com>
2024-10-04 22:46:39 -0400

    brower and poltfill
    two functions

---
+++ brower.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const { AbortController, AbortSignal } =
+    typeof self !== "undefined" ? self :
+    typeof window !== "undefined" ? window :
+    /* otherwise */ undefined
+
+module.exports = AbortController
+module.exports.AbortSignal = AbortSignal
+module.exports.default = AbortController
