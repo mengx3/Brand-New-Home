82497a9da3a47064370a32eb85afe3656b32d61a
syagj <dfdjustin@126.com>
2024-10-11 17:49:53 -0400

    Create abort-controller.mjs
    1

---
+++ abort-controller.mjs
@@ -0,0 +1,10 @@
+import { EventTarget, defineEventAttribute } from 'event-target-shim';
+
+class AbortSignal extends EventTarget {
+    /**
+     * AbortSignal cannot be constructed directly.
+     */
+    constructor() {
+        super();
+        throw new TypeError("AbortSignal cannot be constructed directly");
+    }
