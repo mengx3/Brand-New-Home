d2cc6c48aa39e0a203f89612169b70451c8123e7
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-10-11 20:05:15 -0400

    Update abort-controller.umd.js

--- abort-controller.umd.js
+++ abort-controller.umd.js
@@ -13,3 +13,17 @@
                 ? function (a) {
                       return typeof a;
                   }
+                : function (a) {
+                      return a && "function" == typeof Symbol && a.constructor === Symbol && a !== Symbol.prototype
+                          ? "symbol"
+                          : typeof a;
+                  }),
+        b(a);
+    }
+
+    function c(a, b) {
+        if (!(a instanceof b)) throw new TypeError("Cannot call a class as a function");
+    }
+
+    function d(a, b) {
+        for (var c, d = 0; d < b.length; d++) 
