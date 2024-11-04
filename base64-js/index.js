b8f5cb1cf3ca79acac838acae0f321424a36e32b
EvangelineWang <119088303+EvangelineWang@users.noreply.github.com>
2024-11-04 14:43:41 -0500

    Update index.js

--- base64-js/index.js
+++ base64-js/index.js
@@ -25,3 +25,27 @@ function getLens (b64) {
   if (len % 4 > 0) {
     throw new Error('Invalid string. Length must be a multiple of 4')
   }
+
+  // Trim off extra bytes after placeholder bytes are found
+  // See: https://github.com/beatgammit/base64-js/issues/42
+  var validLen = b64.indexOf('=')
+  if (validLen === -1) validLen = len
+
+  var placeHoldersLen = validLen === len
+    ? 0
+    : 4 - (validLen % 4)
+
+  return [validLen, placeHoldersLen]
+}
+
+// base64 is 4/3 + up to two characters of the original data
+function byteLength (b64) {
+  var lens = getLens(b64)
+  var validLen = lens[0]
+  var placeHoldersLen = lens[1]
+  return ((validLen + placeHoldersLen) * 3 / 4) - placeHoldersLen
+}
+
+function _byteLength (b64, validLen, placeHoldersLen) {
+  return ((validLen + placeHoldersLen) * 3 / 4) - placeHoldersLen
+}
