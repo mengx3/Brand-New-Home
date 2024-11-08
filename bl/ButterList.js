6d37d885b936240fa3db0c2066b5ceadec746dd3
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-11-08 17:22:35 -0500

    Create ButterList.js

---
+++ bl/ButterList.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { Buffer } = require('buffer')
+const symbol = Symbol.for('BufferList')
+
+function BufferList (buf) {
+  if (!(this instanceof BufferList)) {
+    return new BufferList(buf)
+  }
+
+  BufferList._init.call(this, buf)
+}
+
+BufferList._init = function _init (buf) {
+  Object.defineProperty(this, symbol, { value: true })
+
+  this._bufs = []
+  this.length = 0
+
+  if (buf) {
+    this.append(buf)
+  }
+}
