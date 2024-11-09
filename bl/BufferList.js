bdeda2fef60e1815e3a2f674c307359a65b21158
syagj <dfdjustin@126.com>
2024-11-08 21:51:40 -0500

    Update BufferList.js

--- bl/BufferList.js
+++ bl/BufferList.js
@@ -195,3 +195,18 @@ BufferList.prototype.consume = function consume (bytes) {
   bytes = Math.trunc(bytes)
   // do nothing if not a positive number
   if (Number.isNaN(bytes) || bytes <= 0) return this
+
+  while (this._bufs.length) {
+    if (bytes >= this._bufs[0].length) {
+      bytes -= this._bufs[0].length
+      this.length -= this._bufs[0].length
+      this._bufs.shift()
+    } else {
+      this._bufs[0] = this._bufs[0].slice(bytes)
+      this.length -= bytes
+      break
+    }
+  }
+
+  return this
+}
