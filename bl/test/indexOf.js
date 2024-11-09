d4401b9bc2d0c9150c9547cb0d690950a5caa282
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-11-08 21:50:42 -0500

    Update indexOf.js

--- bl/test/indexOf.js
+++ bl/test/indexOf.js
@@ -14,3 +14,38 @@ tape('indexOf single byte needle', (t) => {
 
   t.end()
 })
+
+tape('indexOf multiple byte needle', (t) => {
+  const bl = new BufferList(['abcdefg', 'abcdefg'])
+
+  t.equal(bl.indexOf('ef'), 4)
+  t.equal(bl.indexOf('ef', 5), 11)
+
+  t.end()
+})
+
+tape('indexOf multiple byte needles across buffer boundaries', (t) => {
+  const bl = new BufferList(['abcdefg', 'abcdefg'])
+
+  t.equal(bl.indexOf('fgabc'), 5)
+
+  t.end()
+})
+
+tape('indexOf takes a Uint8Array search', (t) => {
+  const bl = new BufferList(['abcdefg', 'abcdefg'])
+  const search = new Uint8Array([102, 103, 97, 98, 99]) // fgabc
+
+  t.equal(bl.indexOf(search), 5)
+
+  t.end()
+})
+
+tape('indexOf takes a buffer list search', (t) => {
+  const bl = new BufferList(['abcdefg', 'abcdefg'])
+  const search = new BufferList('fgabc')
+
+  t.equal(bl.indexOf(search), 5)
+
+  t.end()
+})
