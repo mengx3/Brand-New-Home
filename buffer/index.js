e9cd7e4fc918ab67ad55eccbbacb2f58f034aca0
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-11-23 20:51:43 -0500

    Update index.js

--- buffer/index.js
+++ buffer/index.js
@@ -1643,3 +1643,23 @@ Buffer.prototype.copy = function copy (target, targetStart, start, end) {
   // Copy 0 bytes; we're done
   if (end === start) return 0
   if (target.length === 0 || this.length === 0) return 0
+
+  // Fatal error conditions
+  if (targetStart < 0) {
+    throw new RangeError('targetStart out of bounds')
+  }
+  if (start < 0 || start >= this.length) throw new RangeError('Index out of range')
+  if (end < 0) throw new RangeError('sourceEnd out of bounds')
+
+  // Are we oob?
+  if (end > this.length) end = this.length
+  if (target.length - targetStart < end - start) {
+    end = target.length - targetStart + start
+  }
+
+  const len = end - start
+
+  if (this === target && typeof Uint8Array.prototype.copyWithin === 'function') {
+    // Use built-in when available, missing from IE11
+    this.copyWithin(targetStart, start, end)
+  } 
