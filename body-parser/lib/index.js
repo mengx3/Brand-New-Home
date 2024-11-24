9675a7a09b1fc6c5cd4d9ae2b2f7e850ea1813f5
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-11-23 20:45:36 -0500

    Create index.js

---
+++ body-parser/lib/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ * @private
+ */
+
+var deprecate = require('depd')('body-parser')
+
+/**
+ * Cache of loaded parsers.
+ * @private
+ */
+
+var parsers = Object.create(null)
+
+/**
+ * @typedef Parsers
+ * @type {function}
+ * @property {function} json
+ * @property {function} raw
+ * @property {function} text
+ * @property {function} urlencoded
+ */
+
+/**
+ * Module exports.
+ * @type {Parsers}
+ */
